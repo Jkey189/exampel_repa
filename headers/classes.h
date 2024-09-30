dc7a0b77a445f6c8c6fe598cfafc2652fc3188c8
Jkey189 <olegator5496@gmail.com>
2024-10-01 01:47:28 +0300

    Move classes to headers/

---
+++ headers/classes.h
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class Base {
+public:
+  Base() = default;
+  Base(int x, int y) :x_(x), y_(y) {}
+  virtual ~Base() {
+    std::cout << "Dead Base class!" << std::endl;
+  }
+
+
+private:
+  int x_;
+  int y_;
+};
+
+class Derived : public Base {
+public:
+  Derived() = default;
+  Derived(int z) : z_(z) {}
+  Derived(int x, int y, int z) : Base(x, y), z_(z) {}
+  virtual ~Derived() {
+    std::cout << "Dead Derived class!" << std::endl;
+  }
+
+  int printValue() const { return z_; }
+
+
+private:
+  int z_;
+};
